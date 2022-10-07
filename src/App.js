1ee7ffeca9bbb5fff7fd12616486636da1cdfa8d
Emmanuel <ejaramilo@hotmail.com>
2022-10-07 10:22:42 -0500

    153 components update in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState();
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph)
+  };
+
+  // the fact that state changes, makes the function component to re run and then
+  // it makes the DOM to gain a new paragraph node
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new</p>}
+      <Button onClick={toggleParagraphHandler}>Show paragraph</Button>
     </div>
   );
 }
